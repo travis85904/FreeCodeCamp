c919dadbdad3645be51e5c04247d95221da55a61
travis85904 <travis85904@gmail.com>
2017-03-24 18:01:16 -0700

    title case

--- Misc.js
+++ Misc.js
@@ -1,4 +1,4 @@
-//get sum of array
+//Get sum of array
 var array = [4, 5, 6, 7, 8];
 var singleVal = 0;
 singleVal = array.reduce(function (previousVal, currentVal) {
@@ -6,6 +6,19 @@ singleVal = array.reduce(function (previousVal, currentVal) {
 }, 0);
 
 
-//return all values less than 6
+//Return all values less than 6
 var oldArray = [1,2,3,4,5,6,7,8,9,10];
 var newArray = oldArray.filter(function(val){return val < 6;});
+
+
+//Title case a sentence
+function titleCase(str) {
+    var strArray = str.toLowerCase().split(" ");
+    for(var i = 0; i < strArray.length; i++){
+        strArray[i] = strArray[i].charAt(0).toUpperCase() + strArray[i].substring(1);
+    }
+    str = strArray.join(" ");
+    return str;
+}
+
+document.write(titleCase("I'm a little tea pot"));
