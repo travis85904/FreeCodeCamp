77beb8f0fac16b2f8de95291d8086f193bca4c53
travis85904 <travis85904@gmail.com>
2017-03-24 21:50:39 -0700

    title case

--- Misc.js
+++ Misc.js
@@ -22,3 +22,32 @@ function titleCase(str) {
 }
 
 document.write(titleCase("I'm a little tea pot"));
+
+
+//Largest of four
+function largestOfFour(arr) {
+    var largest = [0, 0, 0, 0];
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = 0; j < arr[i].length; j++) {
+            if (arr[i][j] > largest[i]) {
+                largest[i] = arr[i][j];
+            }
+        }
+    }
+    return largest;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+
+//Check string ending
+function confirmEnding(str, target) {
+    if (str.substr(-target.length) === target)//substr, index to start from. ('-target.length' is treated as str.length-target.length.)
+        return true;
+
+    return false;
+}
+
+document.write(confirmEnding("Bastian", "n"));
+
+
